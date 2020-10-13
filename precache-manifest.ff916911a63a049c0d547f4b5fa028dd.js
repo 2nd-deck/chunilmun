0628a2af5f235df1a8fbc2f3530a272046b1204b
2nd-deck <archness@naver.com>
2020-10-13 20:09:23 +0900

    Updates

---
+++ precache-manifest.ff916911a63a049c0d547f4b5fa028dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc1368bcf64c18eb129546c5d6c6d32",
+    "url": "/chunilmun/index.html"
+  },
+  {
+    "revision": "452f9a43996237ea268a",
+    "url": "/chunilmun/static/css/main.99f11218.chunk.css"
+  },
+  {
+    "revision": "b9737634c0185c19ea70",
+    "url": "/chunilmun/static/js/2.621d1ea6.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/chunilmun/static/js/2.621d1ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452f9a43996237ea268a",
+    "url": "/chunilmun/static/js/main.a0b9b727.chunk.js"
+  },
+  {
+    "revision": "2326ac7cf5173c4977fb",
+    "url": "/chunilmun/static/js/runtime-main.a6eb8293.js"
+  }
+]);
